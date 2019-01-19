9b89a0d3f5a3b6bc6ab7b8d1b5db94a283660f82
Sugizaki Yukimasa <i.can.speak.c.and.basic@gmail.com>
2019-01-20 00:27:26 +0900

    .bashrc.d/ssh.sh: Include hostname in names of SSH sockets
    ... to avoid conflicts on hosts which shares a home directory.

--- .bashrc.d/ssh.sh
+++ .bashrc.d/ssh.sh
@@ -1,15 +1,19 @@
 # ~/.bashrc.d/ssh.sh
 
-agent="$HOME/.ssh/agent"
+agent="$HOME/.ssh/agent.$(hostname -s)"
 if [ -S "$SSH_AUTH_SOCK" ]; then
-    case "$SSH_AUTH_SOCK" in
-        /tmp/ssh-*/agent.[0-9]*)
-            echo "A live SSH socket $SSH_AUTH_SOCK is found"
-            ln -snf "$SSH_AUTH_SOCK" "$agent"
-            export SSH_AUTH_SOCK="$agent"
-    esac
+    if [ "$SSH_AUTH_SOCK" != "$agent" ]; then
+        if [ -S "$agent" ]; then
+            # Override a sock which may have been lost.
+            echo "Renewing SSH socket"\
+                    "which is already linked in another session"
+        fi
+        echo "Linking SSH socket $SSH_AUTH_SOCK"
+        ln -snf "$SSH_AUTH_SOCK" "$agent"
+        SSH_AUTH_SOCK="$agent"
+    fi
 elif [ -S "$agent" ]; then
-    echo "A live SSH socket is not found but default agent $agent is found"
+    echo "Linked SSH socket $agent is found"
     export SSH_AUTH_SOCK="$agent"
 else
     echo "SSH agent nou found"
