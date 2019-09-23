256a92c1181b9f2c883765bc99f64dc9cf3c0dcd
Sugizaki Yukimasa <sugizaki@hpcs.cs.tsukuba.ac.jp>
2019-09-23 23:33:23 +0900

    .bashrc.d/ssh.sh: Do not override existing agent

--- .bashrc.d/ssh.sh
+++ .bashrc.d/ssh.sh
@@ -1,19 +1,24 @@
 # ~/.bashrc.d/ssh.sh
 
+if ! [[ "$-" =~ i ]]; then
+    return
+fi
+
 agent="$HOME/.ssh/agent.$(hostname -s)"
 if [ -S "$SSH_AUTH_SOCK" ]; then
     if [ "$SSH_AUTH_SOCK" != "$agent" ]; then
         if [ -S "$agent" ]; then
-            # Override a sock which may have been lost.
-            echo "Renewing SSH socket"\
-                    "which is already linked in another session"
+            echo "Using existing SSH socket $agent instead of $SSH_AUTH_SOCK"
+            echo "To renew one, do:"
+            echo "ln -snf $SSH_AUTH_SOCK $agent"
+        else
+            echo "Linking SSH socket $SSH_AUTH_SOCK to $agent"
+            ln -snf "$SSH_AUTH_SOCK" "$agent"
         fi
-        echo "Linking SSH socket $SSH_AUTH_SOCK"
-        ln -snf "$SSH_AUTH_SOCK" "$agent"
         SSH_AUTH_SOCK="$agent"
     fi
 elif [ -S "$agent" ]; then
-    echo "Linked SSH socket $agent is found"
+    echo "Using existing SSH socket $agent"
     export SSH_AUTH_SOCK="$agent"
 else
     echo "SSH agent nou found"
